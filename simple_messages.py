70b6f7858d5f06c3d12e01e4b529df0a8fcf770d
Basavaraj Kalloli <basavaraj.kalloli@springernature.com>
2024-10-14 12:15:32 +0530

    playing around with map function and list

---
+++ simple_messages.py
@@ -0,0 +1,13 @@
+messages = ["Hello", "World"]
+result = " ".join(messages)
+
+for message in messages:
+    print(message)
+
+print("Result is % s!" % result)
+
+nums = [1, 2, 3]
+
+incremented_array = map(lambda x: x + 1, nums)
+
+print(list(incremented_array))
